132a78ed4ceb907830261ef5e8c40e08ff6f938a
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-11-25 23:59:56 +0330

    refactoring Main calss and using log4j.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,35 +9,57 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-public class RcDemo {
+public class Main {
+    private final static Logger log = Logger.getLogger(Main.class);
+
     private static WebDriver driver;
     private static Properties prop = new Properties();
     private static String loginUrl;
     private static String logoutUrl;
 
     static {
-        try (InputStream input = new FileInputStream("src/main/resources/conf.properties")) {
+
+    }
+
+    private static void loadProperties(String propertyFilePath) {
+        try (InputStream input = new FileInputStream(propertyFilePath)) {
 
             prop.load(input);
 
-            String browser = prop.getProperty("browser");
-
-            if("Firefox".equals(browser))
+            if ("Firefox".equals(prop.getProperty("browser")))
                 driver = new FirefoxDriver(); // as you can see it's firefox :)
             else
                 driver = new PhantomJSDriver(); // load browser without ui
 
+            log.info("browser " + prop.getProperty("browser") + " was chosen");
+
             loginUrl = prop.getProperty("loginUrlPath");
+            log.info("loginUrl : " + loginUrl );
+
             logoutUrl = prop.getProperty("logoutUrlPath");
+            log.info("logoutUrl : " + logoutUrl);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
+    private static void initialCheck(int argsLength) {
+        if (argsLength <= 0) {
+            log.error("You should provide config file path");
+            System.exit(-1);
+        }
+
         // todo check selenium and start it :)
         // todo check Phantom and firefox
+    }
+
+    public static void main(String[] args) {
+
+        initialCheck(args.length);
+
+        loadProperties(args[0]);
+
         // todo get command
         try {
 
